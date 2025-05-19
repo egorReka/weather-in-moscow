2c165c37e0b22a9e25745aff4debba89645a6673
egorReka <e.reka55@yandex.ru>
2025-05-19 18:48:18 +0300

    style(Weather): set width to 100% for WeatherContainer

--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -8,7 +8,7 @@ const WeatherContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  // min-height: 100vh;
+  width: 100%;
   padding: 20px;
   background-color: #f5f5f5;
 `;
